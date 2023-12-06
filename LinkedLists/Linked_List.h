35cd0f7f5e2ed433b4d4db1a761bad8998da8ce9
jacky_tsai <jacky_tsai@cmoney.com.tw>
2023-12-06 23:07:40 +0800

    finish LinkedList.Clear and LinkedList.Reverse

--- LinkedLists/Linked_List.h
+++ LinkedLists/Linked_List.h
@@ -25,6 +25,21 @@ public:
     void Print_List();
     // 在 Linked List 內找尋特定資料，並回傳是第幾筆
     int Search_List(T);
+    // 新增插入
+    // 在特定位置新增資料
+    // 新增首項元素
+    void Push_Front(T);
+    // 新增尾端元素
+    void Push_Back(T);
+    // 在特定位置刪除資料
+    // 刪除首項元素
+    void Pop_Front();
+    // 刪除尾端元素
+    void Pop_Back();
+    // 清空所有資料
+    void Clear();
+    // 反轉鏈結串列
+    void Reverse();
 };
 
 template <typename T>
@@ -36,13 +51,19 @@ Linked_List<T>::Linked_List()
 template <typename T>
 void Linked_List<T>::Print_List()
 {
+    if (First == nullptr)
+    {
+        cout << "This list is empty!" << endl;
+        return;
+    }
     Node<T> *cur = First;
-    while (cur != nullptr)
+    cout << "Data: [";
+    while (cur->Next != nullptr)
     {
         cout << cur->Data << " ";
         cur = cur->Next;
     }
-    cout << endl;
+    cout << cur->Data << "]" << endl;
 }
 
 template <typename T>
@@ -60,6 +81,102 @@ int Linked_List<T>::Search_List(T target)
         cur = cur->Next;
     }
     return res;
+}
+
+template <typename T>
+void Linked_List<T>::Push_Front(T value)
+{
+    Node<T> *newNode = new Node<T>{value, First};
+    First = newNode;
+}
+
+template <typename T>
+void Linked_List<T>::Push_Back(T value)
+{
+    if (First == nullptr)
+    {
+        Node<T> *newNode = new Node<T>{value, First};
+        First = newNode;
+    }
+    else
+    {
+        Node<T> *cur = First;
+        while (cur->Next != nullptr)
+        {
+            cur = cur->Next;
+        }
+        cur->Next = new Node<T>{value, nullptr};
+    }
+}
+
+template <typename T>
+void Linked_List<T>::Pop_Front()
+{
+    if (First == nullptr)
+        return;
+    Node<T> *temp = First;
+    First = First->Next;
+    delete temp;
+}
+
+template <typename T>
+void Linked_List<T>::Pop_Back()
+{
+    if (First == nullptr)
+        return;
+    if (First->Next == nullptr)
+    {
+        delete First;
+        First = nullptr;
+        return;
+    }
+    Node<T> *cur = First;
+    while (cur->Next->Next != nullptr)
+    {
+        cur = cur->Next;
+    }
+    delete cur->Next;
+    cur->Next = nullptr;
+}
+
+template <typename T>
+void Linked_List<T>::Clear()
+{
+    while (First != nullptr)
+    {
+        auto temp = First;
+        First = First->Next;
+        delete temp;
+    }
+}
+
+template <typename T>
+void Linked_List<T>::Reverse()
+{
+    if (First == nullptr || First->Next == nullptr)
+        return;
+    Node<T> *cur = new Node<T>{0, First};
+    Node<T> *tail = First;
+    while (tail->Next != nullptr)
+    {
+        auto temp = cur->Next;
+        cur->Next = tail->Next;
+        tail->Next = tail->Next->Next;
+        cur->Next->Next = temp;
+    }
+    First = cur->Next;
+    // Node<T> *previous = nullptr;
+    // Node<T> *cur = First;
+    // Node<T> *preceding = cur->Next;
+    // while (preceding != nullptr)
+    // {
+    //     cur->Next = previous;
+    //     previous = cur;
+    //     cur = preceding;
+    //     preceding = preceding->Next;
+    // }
+    // cur->Next = previous;
+    // First = cur;
 }
 
 #endif // VECTOR_H_INCLUDED
--- LinkedLists/main.cpp
+++ LinkedLists/main.cpp
@@ -6,7 +6,44 @@ using namespace std;
 int main()
 {
     Linked_List<int> List;
+    List.Push_Front(1);
+    List.Push_Front(2);
+    List.Push_Front(3);
     List.Print_List();
-    cout << List.Search_List(1);
+    cout << List.Search_List(1) << endl;
+    List.Pop_Front();
+    List.Pop_Front();
+    List.Print_List();
+    List.Pop_Front();
+    List.Print_List();
+    cout << List.Search_List(1) << endl;
+
+    List.Push_Back(1);
+    List.Push_Back(2);
+    List.Push_Back(3);
+    List.Print_List();
+    cout << List.Search_List(1) << endl;
+    List.Pop_Back();
+    List.Print_List();
+    List.Pop_Back();
+    List.Print_List();
+    List.Pop_Back();
+    List.Print_List();
+    List.Pop_Back();
+    List.Print_List();
+
+    List.Push_Back(1);
+    List.Push_Back(2);
+    List.Push_Back(3);
+    List.Print_List();
+    List.Clear();
+    List.Print_List();
+
+    List.Push_Back(1);
+    List.Push_Back(2);
+    List.Push_Back(3);
+    List.Print_List();
+    List.Reverse();
+    List.Print_List();
     return 0;
 }
