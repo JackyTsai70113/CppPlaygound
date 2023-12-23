9d244b175adf3ea693e4ac04d6fe8eb8bfe68361
jacky_tsai <jacky_tsai@cmoney.com.tw>
2023-12-23 15:07:52 +0800

    HW2 Q4 and Q5

--- HW/HW2/Q5/main.cpp
+++ HW/HW2/Q5/main.cpp
@@ -1,126 +1,49 @@
-#include <algorithm>
-#include <fstream>
 #include <iostream>
-#include <list>
-#include <time.h>
+#include <string>
 #include <vector>
 
 using namespace std;
 
+string Tornado(int n, int dir, vector<vector<int>> v)
+{
+    vector<vector<int>> dirs{{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+
+    int x = n / 2, y = n / 2, i = 0;
+    string res = "";
+    while (x >= 0 && y >= 0 && x < n && y < n)
+    {
+        for (int j = 0; j < i / 2 + 1; ++j)
+        {
+            res += to_string(v[x][y]);
+            x += dirs[dir][0];
+            y += dirs[dir][1];
+        }
+
+        i++;
+        dir = (dir + 1) % 4;
+    }
+    return res;
+}
+
 int main()
 {
-    vector<int> vec{1, 2, 3};
-    list<int> lis{1, 2, 3};
-    clock_t s, f;
-
-    ifstream file_1("Homework_2_1_1.txt");
-    ifstream file_2("Homework_2_1_2.txt");
-
-    list<int>::iterator iter_list;
-    vector<int>::iterator iter_vec;
-    int index, number;
-    // A.	Insert by index
-    if (file_1)
+    int n;
+    cin >> n;
+    int dir = 0;
+    cin >> dir;
+    vector<vector<int>> vv;
+    for (int i = 0; i < n; i++)
     {
-        s = clock();
-        while (file_1 >> index >> number)
+        vector<int> v;
+        for (int j = 0; j < n; j++)
         {
-            vec.insert(vec.begin() + index, number);
+            int num;
+            cin >> num;
+            v.push_back(num);
         }
-        f = clock();
-        cout << "(Vector) Time consumption for insert by index: " << (f - s) / (double)CLOCKS_PER_SEC << " seconds" << endl;
-
-        file_1.clear(); // clear bad state after eof
-        file_1.seekg(0);
-        list<int>::iterator iter;
-        s = clock();
-        while (file_1 >> index >> number)
-        {
-            iter = lis.begin();
-            advance(iter, index);
-            lis.insert(iter, number);
-        }
-        f = clock();
-        cout << "(list) Time consumption for insert by index: " << (f - s) / (double)CLOCKS_PER_SEC << " seconds" << endl;
+        vv.push_back(v);
     }
-    else
-        cout << "File not exist!" << endl;
-
-    // Reset vector and list
-    vec = {1, 2, 3};
-    lis = {1, 2, 3};
-
-    // B.	Insert by value
-    if (file_2)
-    {
-        s = clock();
-        while (file_2 >> index >> number)
-        {
-            iter_vec = find(vec.begin(), vec.end(), index);
-            vec.insert(iter_vec, number);
-        }
-        f = clock();
-        cout << "(Vector) Time consumption for insert by value: " << (f - s) / (double)CLOCKS_PER_SEC << " seconds" << endl;
-
-        file_2.clear(); // clear bad state after eof
-        file_2.seekg(0);
-        s = clock();
-        while (file_2 >> index >> number)
-        {
-            iter_list = find(lis.begin(), lis.end(), index);
-            lis.insert(iter_list, number);
-        }
-        f = clock();
-        cout << "(list) Time consumption for insert by value: " << (f - s) / (double)CLOCKS_PER_SEC << " seconds" << endl;
-    }
-    else
-        cout << "File not exist!" << endl;
-    file_1.close();
-    file_2.close();
-
-    int N = 1000000;
-
-    // Reset vector and list
-    vec.clear();
-    lis.clear();
-
-    // C.	Push Front
-    s = clock();
-    for (int i = 0; i < N; i++)
-    {
-        vec.insert(vec.begin(), i);
-    }
-    f = clock();
-    cout << "(Vector) Time consumption for insert " << N << " integer on the front: " << (f - s) / (double)CLOCKS_PER_SEC << " seconds" << endl;
-
-    s = clock();
-    for (int i = 0; i < N; i++)
-    {
-        lis.push_front(i);
-    }
-    f = clock();
-    cout << "(list) Time consumption for insert " << N << " integer on the front: " << (f - s) / (double)CLOCKS_PER_SEC << " seconds" << endl;
-
-    // Reset vector and list
-    vec.clear();
-    lis.clear();
-
-    // D.	Push Back
-    s = clock();
-    for (int i = 0; i < N; i++)
-    {
-        vec.push_back(i);
-    }
-    f = clock();
-    cout << "(Vector) Time consumption for insert " << N << " integer on the end: " << (f - s) / (double)CLOCKS_PER_SEC << " seconds" << endl;
-
-    s = clock();
-    for (int i = 0; i < N; i++)
-    {
-        lis.push_back(i);
-    }
-    f = clock();
-    cout << "(list) Time consumption for insert " << N << " integer on the end: " << (f - s) / (double)CLOCKS_PER_SEC << " seconds" << endl;
-
+    auto actual = Tornado(n, dir, vv);
+    cout << actual << endl;
     return 0;
 }
