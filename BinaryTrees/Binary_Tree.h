da8638f708db8f26c0a0d3d21790a412846c0f47
jacky_tsai <jacky_tsai@cmoney.com.tw>
2024-01-06 01:14:27 +0800

    feat: add Binary_Tree.h

--- BinaryTrees/Binary_Tree.h
+++ BinaryTrees/Binary_Tree.h
@@ -2,7 +2,7 @@
 #define BINARY_TREE_H_INCLUDED
 
 #include <iostream>
-#include <stack>
+#include <queue>
 
 using namespace std;
 
@@ -11,6 +11,7 @@ struct Node
 {
     int index;
     T data;
+    Node<T> *parent;
     Node<T> *left;
     Node<T> *right;
 };
@@ -32,7 +33,22 @@ public:
     Node<T> *Search(int index);
     // 刪除資料
     bool Delete(int);
-    // 尋訪: 前序中序後序
+    // 前序尋訪
+    void PreOrder(Node<T> *);
+    // 中序尋訪
+    void InOrder(Node<T> *);
+    // 後序尋訪
+    void PostOrder(Node<T> *);
+    // level order 尋訪
+    void LevelOrder(Node<T> *);
+    // 最左節點
+    Node<T> *Left_Most(Node<T> *);
+    // 最右節點
+    Node<T> *Right_Most(Node<T> *);
+    // 前任，往左一個節點
+    Node<T> *Predecessor(Node<T> *);
+    // 接班人，往右一個節點
+    Node<T> *Successor(Node<T> *);
 };
 
 template <typename T>
@@ -65,6 +81,7 @@ bool Binary_Tree<T>::Insert(int index, T data)
         {
             if (cur->left == nullptr)
             {
+                newNode->parent = cur;
                 cur->left = newNode;
                 return true;
             }
@@ -74,6 +91,7 @@ bool Binary_Tree<T>::Insert(int index, T data)
         {
             if (cur->right == nullptr)
             {
+                newNode->parent = cur;
                 cur->right = newNode;
                 return true;
             }
@@ -99,9 +117,207 @@ Node<T> *Binary_Tree<T>::Search(int targetIndex)
 }
 
 template <typename T>
-bool Binary_Tree<T>::Delete(int)
+bool Binary_Tree<T>::Delete(int targetIndex)
 {
-    return false;
+    Node<T> *delete_node = Search(targetIndex);
+    if (delete_node == nullptr)
+    {
+        return false;
+    }
+
+    // case 1. delete_node is a leaf node
+    if (delete_node->left == nullptr && delete_node->right == nullptr)
+    {
+        if (delete_node == root)
+        {
+            root = nullptr;
+        }
+        if (delete_node == delete_node->parent->left)
+        {
+            cout << "left";
+            delete_node->parent->left = nullptr;
+        }
+        else
+        {
+            cout << "right";
+            delete_node->parent->right = nullptr;
+        }
+        delete delete_node;
+    }
+    // case 2. delete_node has 1 child node
+    else if (delete_node->left == nullptr || delete_node->right == nullptr)
+    {
+        Node<T> *child = delete_node->left != nullptr ? delete_node->left : delete_node->right;
+        if (delete_node == root)
+        {
+            root = child;
+        }
+        if (delete_node == delete_node->parent->left)
+        {
+            delete_node->parent->left = child;
+        }
+        else
+        {
+            delete_node->parent->right = child;
+        }
+        delete delete_node;
+    }
+    // case 3. delete_node has 2 child node
+    else if (delete_node->left != nullptr && delete_node->right != nullptr)
+    {
+        Node<T> *successor = Left_Most(delete_node->right);
+        // successor must has 0 or 1 child
+        T successorData = successor->data;
+        int successorIndex = successor->index;
+        Delete(successor->index);
+        delete_node->data = successorData;
+        delete_node->index = successorIndex;
+    }
+    return true;
 }
 
+template <typename T>
+void Binary_Tree<T>::PreOrder(Node<T> *node)
+{
+    if (node == nullptr)
+        return;
+    cout << node->index << ", ";
+    if (node->left != nullptr)
+    {
+        PreOrder(node->left);
+    }
+    if (node->right != nullptr)
+    {
+        PreOrder(node->right);
+    }
+}
+
+template <typename T>
+void Binary_Tree<T>::InOrder(Node<T> *node)
+{
+    if (node == nullptr)
+        return;
+    if (node->left != nullptr)
+    {
+        InOrder(node->left);
+    }
+    cout << node->index << ", ";
+    if (node->right != nullptr)
+    {
+        InOrder(node->right);
+    }
+}
+
+template <typename T>
+void Binary_Tree<T>::PostOrder(Node<T> *node)
+{
+    if (node == nullptr)
+        return;
+    if (node->left != nullptr)
+    {
+        PostOrder(node->left);
+    }
+    if (node->right != nullptr)
+    {
+        PostOrder(node->right);
+    }
+    cout << node->index << ", ";
+}
+
+template <typename T>
+void Binary_Tree<T>::LevelOrder(Node<T> *node)
+{
+    if (node == nullptr)
+        return;
+    queue<Node<T> *> q;
+    q.push(node);
+    while (!q.empty())
+    {
+        for (int i = q.size(); i > 0; i--)
+        {
+            Node<T> *n = q.front();
+            q.pop();
+            cout << n->index << ", ";
+            if (n->left != nullptr)
+            {
+                q.push(n->left);
+            }
+            if (n->right != nullptr)
+            {
+                q.push(n->right);
+            }
+        }
+    }
+}
+
+template <typename T>
+Node<T> *Binary_Tree<T>::Left_Most(Node<T> *node)
+{
+    if (node == nullptr)
+    {
+        return nullptr;
+    }
+    auto result = node;
+    while (result->left != nullptr)
+    {
+        result = result->left;
+    }
+    return result;
+}
+
+template <typename T>
+Node<T> *Binary_Tree<T>::Right_Most(Node<T> *node)
+{
+    if (node == nullptr)
+    {
+        return nullptr;
+    }
+    auto result = node;
+    while (result->right != nullptr)
+    {
+        result = result->right;
+    }
+    return result;
+}
+
+template <typename T>
+Node<T> *Binary_Tree<T>::Predecessor(Node<T> *node)
+{
+    if (node == nullptr)
+    {
+        return nullptr;
+    }
+    if (node->left != nullptr)
+    {
+        return Right_Most(node->left);
+    }
+
+    Node<T> *cur = node->parent;
+    while (cur != nullptr && cur->right != node)
+    {
+        node = cur;
+        cur = cur->parent;
+    }
+    return cur;
+}
+
+template <typename T>
+Node<T> *Binary_Tree<T>::Successor(Node<T> *node)
+{
+    if (node == nullptr)
+    {
+        return nullptr;
+    }
+    if (node->right != nullptr)
+    {
+        return Left_Most(node->right);
+    }
+    Node<T> *cur = node->parent;
+    while (cur != nullptr && cur->left != node)
+    {
+        node = cur;
+        cur = cur->parent;
+    }
+    return cur;
+}
 #endif // BINARY_TREE_H_INCLUDED
--- BinaryTrees/main.cpp
+++ BinaryTrees/main.cpp
@@ -2,16 +2,82 @@
 #include <iostream>
 
 using namespace std;
+void TestTraverse()
+{
+    Binary_Tree<int> tree;
+    tree.Insert(58, 58);
+    tree.Insert(24, 6);
+    tree.Insert(97, 8);
+    tree.Insert(12, 3);
+    tree.Insert(49, 6);
+    tree.Insert(82, 8);
+    tree.Insert(55, 8);
+
+    tree.Print(*tree.root);
+    cout << "Pre-Order: ";
+    tree.PreOrder(tree.root);
+    cout << endl;
+
+    cout << "In-Order: ";
+    tree.InOrder(tree.root);
+    cout << endl;
+
+    cout << "Post-Order: ";
+    tree.PostOrder(tree.root);
+    cout << endl;
+
+    cout << "Level-Order: ";
+    tree.LevelOrder(tree.root);
+    cout << endl;
+
+    cout << "Left most of 58: " << tree.Left_Most(tree.Search(58))->index << endl;
+    cout << "Right most of 58: " << tree.Right_Most(tree.Search(58))->index << endl;
+    cout << "Predecessor of 58: " << tree.Predecessor(tree.Search(58))->index << endl;
+    cout << "Successor of 58: " << tree.Successor(tree.Search(58))->index << endl;
+    cout << "Predecessor of 82: " << tree.Predecessor(tree.Search(82))->index << endl;
+    cout << "Successor of 55: " << tree.Successor(tree.Search(55))->index << endl;
+}
+
+void TestDelete()
+{
+    Binary_Tree<string> tree;
+    tree.Insert(58, "A");
+    tree.Insert(24, "B");
+    tree.Insert(97, "C");
+    tree.Insert(12, "D");
+    tree.Insert(49, "E");
+    tree.Insert(82, "F");
+    tree.Insert(55, "G");
+
+    tree.Delete(58);
+
+    tree.Print(*tree.root);
+    cout << "Pre-Order: ";
+    tree.PreOrder(tree.root);
+    cout << endl;
+
+    cout << "In-Order: ";
+    tree.InOrder(tree.root);
+    cout << endl;
+
+    cout << "Post-Order: ";
+    tree.PostOrder(tree.root);
+    cout << endl;
+
+    cout << "Level-Order: ";
+    tree.LevelOrder(tree.root);
+    cout << endl;
+
+    cout << "Left most of 24: " << tree.Left_Most(tree.Search(24))->index << endl;
+    cout << "Right most of 24: " << tree.Right_Most(tree.Search(24))->index << endl;
+    cout << "Successor of 55: " << tree.Successor(tree.Search(55))->index << endl;
+    cout << "Predecessor of 82: " << tree.Predecessor(tree.Search(82))->index << endl;
+}
 
 int main()
 {
-    Binary_Tree<int> tree;
-    tree.Insert(2, 3);
-    tree.Insert(3, 6);
-    tree.Insert(7, 8);
-    tree.Print(*tree.root);
-    tree.Print(*(tree.root->right));
-    auto found = tree.Search(7);
-    tree.Print(*found);
+    // TestTraverse();
+    TestDelete();
+
     return 0;
 }
